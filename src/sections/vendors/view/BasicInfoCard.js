1f3d3c445328b5bac134b406ee6b69c579cfb282
Raghav Kattel <arg.ahv7@gmail.com>
2023-02-09 16:11:34 +0545

    vendor ui changes

--- src/sections/vendors/view/BasicInfoCard.js
+++ src/sections/vendors/view/BasicInfoCard.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
-import { Card, CardContent, Chip, Grid, Stack, Typography } from '@mui/material';
+import { Alert, Card, CardContent, CardHeader, Chip, Grid, Stack, Typography } from '@mui/material';
 import { useVendorsContext } from '@contexts/vendors';
 // import truncateEthAddress from '@utils/truncateEthAddress';
 import { useAuthContext } from 'src/auth/useAuthContext';
 import WalletExplorerButton from '@components/button/WalletExplorerButton';
+import SendToken from './SendToken';
 
 BasicInfoCard.propTypes = {
   chainData: PropTypes.object,
@@ -11,43 +12,36 @@ BasicInfoCard.propTypes = {
 
 export default function BasicInfoCard({ chainData }) {
   const { singleVendor } = useVendorsContext();
-  const { roles } = useAuthContext();
 
   return (
     <Card>
+      <CardHeader
+        title={<Typography variant="subtitle1">{singleVendor?.name}</Typography>}
+        action={
+          <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={2}>
+            <SendToken />
+          </Stack>
+        }
+      />
       <CardContent>
-        <Stack direction="row" justifyContent="space-between" alignItems="flex-start" spacing={12}>
-          <Typography variant="h6">
-            {roles.isDonor ? singleVendor?.name : singleVendor?.name?.substring(0, 1) + 'xxxxxxx Xxxxx'}
-          </Typography>
-          {
-            <div>
-              {chainData?.walletAddress && (
-                <Chip
-                  label={chainData?.isActive ? 'Approved' : 'Not Approved'}
-                  color={chainData?.isActive ? 'success' : 'error'}
-                />
-              )}
-            </div>
-          }
-        </Stack>
+        {chainData?.pendingTokens > 0 && (
+          <Alert sx={{ mt: 2 }} type="info">
+            {' '}
+            This distributor has yet to accept {chainData?.pendingTokens} tokens.
+          </Alert>
+        )}
 
-        <Stack sx={{ p: 2 }} direction="row" justifyContent="space-between" alignItems="center" spacing={12}>
+        <Stack sx={{ p: 2 }} direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
           <Grid container direction="column" justifyContent="center" alignItems="flex-start">
-            <Typography variant="body1" sx={{ fontWeight: 600 }}>
+            <Typography variant="subtitle" sx={{ fontWeight: 600 }}>
               {singleVendor?.phone}
             </Typography>
             <Typography variant="body2">Phone</Typography>
           </Grid>
-          <Grid container direction="column" justifyContent="center" alignItems="flex-start">
-            <Typography variant="body1">{singleVendor?.gender}</Typography>
-            <Typography variant="body2">Gender</Typography>
-          </Grid>
-          <Grid container direction="column" justifyContent="center" alignItems="flex-start">
-            <Typography variant="body1" sx={{ fontWeight: 600 }}>
-              <WalletExplorerButton address={singleVendor?.walletAddress} type="address" />
 
-              {/* </WalletExplorerButton> */}
+          <Grid container direction="column" justifyContent="center" alignItems="flex-start">
+            <Typography variant="subtitle" sx={{ fontWeight: 600 }}>
+              <WalletExplorerButton address={singleVendor?.walletAddress} type="address" truncateLength={2} />
             </Typography>
             <Typography variant="body2">Wallet Address</Typography>
           </Grid>
---
+++ src/sections/vendors/view/maps/ClusterMap.js
@@ -0,0 +1,87 @@
+import { useRef, memo } from 'react';
+import Map, { Layer, Source } from 'react-map-gl';
+//
+import { clusterLayer, clusterCountLayer, unclusteredPointLayer } from './layers';
+
+// ----------------------------------------------------------------------
+
+function MapClusters({ mapData = [], ...other }) {
+  const mapRef = useRef(null);
+
+  console.log('mapData', mapData);
+
+  mapData = mapData.map((item) => ({
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [+item?.longitude, +item?.latitude],
+    },
+    properties: {
+      // cluster: true,
+      id: item.name,
+    },
+  }));
+
+  let data = {
+    type: 'FeatureCollection',
+    crs: {
+      type: 'name',
+      properties: {
+        name: 'urn:ogc:def:crs:OGC:1.3:CRS84',
+      },
+    },
+    features: mapData,
+  };
+
+  const onClick = (event) => {
+    const feature = event.features?.[0];
+
+    const clusterId = feature?.properties?.cluster_id;
+
+    const mapboxSource = mapRef.current?.getSource('earthquakes');
+
+    mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
+      if (err) {
+        return;
+      }
+
+      mapRef.current?.easeTo({
+        center: feature?.geometry?.coordinates,
+        zoom: Number.isNaN(zoom) ? 3 : zoom,
+        duration: 500,
+      });
+    });
+  };
+
+  return (
+    <>
+      <Map
+        initialViewState={{
+          longitude: 68.48329,
+          latitude: 28.31456,
+          zoom: 4,
+        }}
+        interactiveLayerIds={[clusterLayer.id || '']}
+        onClick={onClick}
+        ref={mapRef}
+        {...other}
+      >
+        <Source
+          id="earthquakes"
+          type="geojson"
+          data={data}
+          // data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
+          cluster={true}
+          clusterMaxZoom={14}
+          clusterRadius={50}
+        >
+          <Layer {...clusterLayer} />
+          <Layer {...clusterCountLayer} />
+          <Layer {...unclusteredPointLayer} />
+        </Source>
+      </Map>
+    </>
+  );
+}
+
+export default memo(MapClusters);
