a8f93b39359f4735c47fa0d8fc9318ebd5935b5b
Santosh <santosh@rumsan.com>
2023-02-24 15:05:41 +0545

    list tx by vendor

--- src/services/chaincache.js
+++ src/services/chaincache.js
@@ -12,4 +12,13 @@ export const ChainCacheService = {
     let response = await chainCacher.get(`/contracts/${address}/events?names=ClaimAssigned,ClaimProcessed`);
     return response.data;
   },
+
+  listTransactionsByVendor: async (contractAddress, vendorAddress) => {
+    console.log('vendorAddress', vendorAddress);
+    let response = await chainCacher.get(`/contracts/${contractAddress}/events?names=ClaimProcessed`);
+    const rows = response.data.data.rows.filter(
+      (row) => row.params.find((param) => param.name === 'vendor')?.value === vendorAddress.toLowerCase()
+    );
+    return rows;
+  },
 };
