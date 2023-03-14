0d830d6622d4d9927e1c337875b7c79f82ef40de
Raghav Kattel <arg.ahv7@gmail.com>
2023-03-14 17:57:35 +0545

    can connect to wallet using metanmask

--- src/hooks/useWallet.js
+++ src/hooks/useWallet.js
@@ -1,9 +1,18 @@
 import ethers, { providers } from 'ethers';
 import { useAuthContext } from 'src/auth/useAuthContext';
+import useWalletConnection from './useWalletConnection';
 
 export const useWallet = (privateKey) => {
+  const { isWalletConnected, library } = useWalletConnection();
   let { chainUrl, chainId, wallet } = useAuthContext();
+
   if (privateKey) wallet = new ethers.Wallet(privateKey);
+
+  if (isWalletConnected) {
+    return wallet.connect(library);
+  }
+
+  console.log('wallet', wallet);
 
   return wallet.connect(
     new providers.StaticJsonRpcProvider(chainUrl, {
