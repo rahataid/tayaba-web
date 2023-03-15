325e06bd8ce1fa627ba2836db77b5713e99ac6fb
sarvesh-karki <sarvesh.karki@rumsan.net>
2023-03-15 15:20:15 +0545

    connect wallet warning added

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -7,18 +7,32 @@ import { Container, Button, Box } from '@mui/material';
 import { TableContainer } from '@sections/projects';
 import { ProjectProvider } from '@contexts/projects';
 import { useRouter } from 'next/router';
+import useWalletConnection from '@hooks/useWalletConnection';
+import snackbar, { useSnackbar } from '@components/snackbar';
 
 const PAGE_TITLE = 'Projects';
 
 export default function ProjectsList() {
+  const {isWalletConnected} = useWalletConnection();
   const { themeStretch } = useSettingsContext();
   const { push } = useRouter();
+  const snackBar = useSnackbar();
+
+
   const handleAdd = () => {
+    if (!isWalletConnected) {
+      snackBar.enqueueSnackbar('Please connect a wallet.',
+        {
+          variant:'warning'
+        }
+      );
+      return
+    }
     push(`/projects/add`);
   };
   return (
     <ProjectProvider>
-      <Page title={PAGE_TITLE} nocard action={<Button onClick={handleAdd}>Add Project</Button>}>
+      <Page title={PAGE_TITLE} nocard action={<Button onClick={handleAdd} variant="outlined">Add Project</Button>}>
         <Container maxWidth={themeStretch ? false : 'xl'}>
           <TableContainer />
         </Container>
