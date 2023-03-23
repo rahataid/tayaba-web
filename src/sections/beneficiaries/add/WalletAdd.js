5db66518b2e3a478bcffbbc4be5a013b68566f29
unknown <abiskar.rumsan@gmail.com>
2023-03-23 17:27:01 +0545

    minor changes

--- src/sections/beneficiaries/add/WalletAdd.js
+++ src/sections/beneficiaries/add/WalletAdd.js
@@ -18,7 +18,7 @@ const community = [
   },
 ];
 export default function WalletAdd({ beneficaryInfo, setStep }) {
-  const [walletAddress, setwalletAddress] = useState();
+  const [walletAddress, setwalletAddress] = useState('');
 
   const handleCreateWallet = async () => {
     let wallet = Web3Utils.generateWallet('');
@@ -26,46 +26,35 @@ export default function WalletAdd({ beneficaryInfo, setStep }) {
   };
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12} md={12}>
-        <Card>
-          <Grid container padding={1} spacing={3}>
-            <Grid item xs={12} md={6}>
-              <Grid container spacing={1}>
-                <Grid item xs={12} md={9}>
-                  <Stack spacing={1}>
-                    <RHFTextField
-                      id="walletAddress"
-                      value={walletAddress}
-                      name="walletAddress"
-                      label="Wallet Address"
-                    />
-                  </Stack>
-                </Grid>
-                <Grid item xs={12} md={3}>
-                  <Button variant="outlined" onClick={handleCreateWallet}>
-                    Generate Wallet
-                  </Button>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Stack spacing={3}>
-                <RHFSelect name="community" label="Select Community">
-                  {' '}
-                  <option>Select Community</option>
-                  {community
-                    ? community?.map((obj) => (
-                        <option key={obj.value} value={obj.value}>
-                          {obj.label}
-                        </option>
-                      ))
-                    : ''}
-                </RHFSelect>
-              </Stack>
-            </Grid>
+    <Grid container padding={1} spacing={3}>
+      <Grid item xs={12} md={6}>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={9}>
+            <Stack spacing={1}>
+              <RHFTextField id="walletAddress" value={walletAddress} name="walletAddress" label="Wallet Address" />
+            </Stack>
           </Grid>
-        </Card>
+          <Grid item xs={12} md={3}>
+            <Button variant="outlined" onClick={handleCreateWallet}>
+              Generate Wallet
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Stack spacing={3}>
+          <RHFSelect name="community" label="Select Community">
+            {' '}
+            <option>Select Community</option>
+            {community
+              ? community?.map((obj) => (
+                  <option key={obj.value} value={obj.value}>
+                    {obj.label}
+                  </option>
+                ))
+              : ''}
+          </RHFSelect>
+        </Stack>
       </Grid>
     </Grid>
   );
