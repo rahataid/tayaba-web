61df5628412d25ca71cec8798cadc0b52b201009
unknown <abiskar.rumsan@gmail.com>
2023-03-27 11:55:18 +0545

    changed added info

--- src/sections/beneficiaries/add/AddedInfo.js
+++ src/sections/beneficiaries/add/AddedInfo.js
@@ -7,42 +7,40 @@ import moment from 'moment';
 
 export default function AddedInfo({ beneficaryInfo = {} }) {
   return (
-    <>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={12}>
-          <Card>
-            <Stack spacing={3} padding={1}>
-              <Grid container spacing={3}>
-                {Object.entries(beneficaryInfo).map(([key, value]) =>
-                  value instanceof Date ? (
-                    <Grid item xs={12} md={6}>
-                      <Typography paddingLeft={2} variant="h5">
-                        {' '}
-                        {moment(value).format('DD MMM YYYY')}
-                      </Typography>
-                      <Typography paddingLeft={2} variant="body">
-                        {' '}
-                        {key.charAt(0).toUpperCase() + key.slice(1)}
-                      </Typography>
-                    </Grid>
-                  ) : (
-                    <Grid item xs={12} md={6}>
-                      <Typography paddingLeft={2} variant="h5">
-                        {' '}
-                        {value}
-                      </Typography>
-                      <Typography paddingLeft={2} variant="body">
-                        {' '}
-                        {key.charAt(0).toUpperCase() + key.slice(1)}
-                      </Typography>
-                    </Grid>
-                  )
-                )}
-              </Grid>
-            </Stack>
-          </Card>
-        </Grid>
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={12}>
+        <Card>
+          <Stack spacing={3} padding={1}>
+            <Grid container spacing={3}>
+              {Object.entries(beneficaryInfo).map(([key, value]) =>
+                value instanceof Date ? (
+                  <Grid item xs={12} md={6}>
+                    <Typography paddingLeft={2} variant="h5">
+                      {' '}
+                      {moment(value).format('DD MMM YYYY')}
+                    </Typography>
+                    <Typography paddingLeft={2} variant="body">
+                      {' '}
+                      {key.charAt(0).toUpperCase() + key.slice(1)}
+                    </Typography>
+                  </Grid>
+                ) : (
+                  <Grid item xs={12} md={6}>
+                    <Typography paddingLeft={2} variant="h5">
+                      {' '}
+                      {value}
+                    </Typography>
+                    <Typography paddingLeft={2} variant="body">
+                      {' '}
+                      {key.charAt(0).toUpperCase() + key.slice(1)}
+                    </Typography>
+                  </Grid>
+                )
+              )}
+            </Grid>
+          </Stack>
+        </Card>
       </Grid>
-    </>
+    </Grid>
   );
 }
